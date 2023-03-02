3b44f1c32e8b9f252dd62e696023468ada77d2f4
Michalous <49964028+Michalous@users.noreply.github.com>
2023-03-02 11:45:42 +0000

    console.logs deleted

--- index.js
+++ index.js
@@ -35,13 +35,11 @@ function chooseOption() {
                 newEmployee("newIntern")
             }
             else {
-            console.log(answer.options)
-            let newHTML = render(list_of_employees)
-            fs.writeFile(outputPath, newHTML, function (err, file) {
-                if (err) throw err;
-                console.log('Saved!');
-            })
-            console.log(newHTML)
+                let newHTML = render(list_of_employees)
+                fs.writeFile(outputPath, newHTML, function (err, file) {
+                    if (err) throw err;
+                    console.log('Saved!');
+                })
             }
         })
 }
@@ -102,106 +100,13 @@ function appendEmployee(answer, employee) {
     if (employee == 'newManager') {
         let newManager = new Manager(answer.name, answer.id, answer.email, answer.officeNumber)
         list_of_employees.push(newManager)
-        console.log(list_of_employees)
     }
     else if (employee == 'newEngineer') {
         let newEngineer = new Engineer(answer.name, answer.id, answer.email, answer.github)
         list_of_employees.push(newEngineer)
-        console.log(list_of_employees)
     }
     else {
         let newIntern = new Intern(answer.name, answer.id, answer.email, answer.school)
         list_of_employees.push(newIntern)
-        console.log(list_of_employees)
     }
 }
-
-// function init() {
-//     inquirer
-//         .prompt([
-//             {
-//                 name: "title",
-//                 type: "input",
-//                 message: "What is the title of your project?"
-//             },
-//             {
-//                 name: "description",
-//                 type: "input",
-//                 message: "Description (leave empty for none):"
-    
-//             },
-//             {
-//                 name: "installation",
-//                 type: "input",
-//                 message: "Installation instructions:"
-//             },
-//             {
-//                 name: "usage",
-//                 type: "input",
-//                 message: "Usage information:"
-//             },
-//             {
-//                 name: "contribution",
-//                 type: "input",
-//                 message: "Contribution guidelines:"
-//             },
-//             {
-//                 name: "test",
-//                 type: "input",
-//                 message: "Test instructions:"
-//             },
-//             {
-//                 name: "license",
-//                 type: "list",
-//                 message: "License:",
-//                 choices: ["None",
-//                           "Apache License 2.0",
-//                           "GNU General Public License v3.0",
-//                           "MIT License",
-//                           "BSD 2-Clause \"Simplified\" License",
-//                           "BSD 3-Cluase \"New\" or \"Revised\" License",
-//                           "Boost Software License 1.0",
-//                           "Creative Commons Zero v1.0 Universal",
-//                           "Eclipse Public License v3.0",
-//                           "GNU Affero General Public License v3.0",
-//                           "GNU General Public License v2.0",
-//                           "GNU Lesser General Public License v2.1",
-//                           "Mozilla Public License 2.0",
-//                           "The Unlicense"]
-//               },
-//               {
-//                 name: "username",
-//                 type: "input",
-//                 message: "GitHub username:"
-//               },
-//               {
-//                 name: "email",
-//                 type: "input",
-//                 message: "Your email address:"
-//               },
-//         ])
-//         .then((answer) => {
-//             var toc
-//             var toc_count = 0
-//             var table_of_content = [answer.installation, answer.usage, answer.contribution, answer.license]
-//             for (ans of table_of_content) {
-//                 ans ? toc_count++ : toc_count = toc_count
-//             }
-//             toc_count >= 3 ? toc = true : toc = false
-    
-//             var result = generateMarkdown(answer, toc)
-//             fs.writeFile('mynewfile2.md', `${result}`, function (err, file) {
-//                 if (err) throw err;
-//                 console.log('Saved!');
-//               });
-//         })
-// }
-// init()
-
-
-// empl = new Employee("pepa", '23', 'a@a.xz')
-// man = new Manager('jozef', '34', 'a@f.fg', '234')
-
-// console.log(empl.getRole())
-// console.log(man.getRole())
-// console.log(man.officeNumber)
